59e7be3701ea39573045c5d38890165d57a5f47c
Anamul10 <anamul.hasan@mnsu.edu>
2022-08-19 13:53:08 -0400

    first commit

---
+++ js/common.js
@@ -0,0 +1,28 @@
+function getTextElementValueById(elementId) {
+    const phoneTotalElement = document.getElementById(elementId);
+    const currentPhoneTotalString = phoneTotalElement.innerText;
+    const currentPhoneTotal = parseInt(currentPhoneTotalString);
+    return currentPhoneTotal;
+}
+
+function setTextElementValuById(elementId, value) {
+    const subTotalElement = document.getElementById(elementId);
+    subTotalElement.innerText = value;
+}
+
+function calculateSubTotal() {
+    // calculate total 
+    const currentPhoneTotal = getTextElementValueById('phone-total');
+    const currentCaseTotal = getTextElementValueById('case-total');
+
+    const currentSubTotal = currentPhoneTotal + currentCaseTotal;
+    setTextElementValuById('sub-total', currentSubTotal);
+
+    // calculate tax
+    const taxAmountString = (currentSubTotal * 0.1).toFixed(2);
+    const taxAmount = parseFloat(taxAmountString);
+    setTextElementValuById('tax-amount', taxAmount);
+
+    const finalAmount = currentSubTotal + taxAmount;
+    setTextElementValuById('final-total', finalAmount);
+}
